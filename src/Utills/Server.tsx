c8abf1596a86b7f8ca5cbeb79234b01d269b06a3
nileshpanchalnp <nileshpanchal4411@gmail.com>
2025-11-14 15:49:23 +0530

    backend host

--- src/Utills/Server.tsx
+++ src/Utills/Server.tsx
@@ -1,2 +1,3 @@
 // export const Server = "https://stackoverflow-backend-adc8.onrender.com/";
-export const Server = "http://localhost:5000/";
+// export const Server = "http://localhost:5000/";
+export const Server = "https://coderconnect-backend.onrender.com/";
