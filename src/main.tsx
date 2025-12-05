240c38641606ba65e421343ea382cd19966ab657
nileshpanchalnp <nileshpanchal4411@gmail.com>
2025-12-05 14:13:15 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'; // Make sure to import this
+import { BrowserRouter } from 'react-router-dom'; 
 
-// Get the root element
 const root = createRoot(document.getElementById('root')!);
 
-// Render your app
 root.render(
   <BrowserRouter>
     <App />
